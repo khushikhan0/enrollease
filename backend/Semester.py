3e497adc64097c2a0a002ebb6bfe492a8b0c0376
Kyra Holmes <kyra@holmesmail.com>
2024-11-02 18:34:36 -0400

    Added Backend

---
+++ backend/Semester.py
@@ -0,0 +1,16 @@
+'''
+    TO DO:
+    - Count Credits
+    - Check Co-Reqs
+    - Check Overloading 
+    - Override Overload Error
+    - Check PreReqs - (prevSemester)
+    
+'''
+
+class Semester:
+    def __init__(courses):
+        self.courses = courses
+
+    
+    
--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -1,7 +1,22 @@
+import { Button } from '@mui/material';
 import React from 'react';
+
+var message
+
+async function onClick(event) {
+    try {
+        const response = await fetch("http://127.0.0.1:5000/api/courses");
+        const data = await response.json();
+        
+        // Log the string directly from the backend
+        console.log(data.courses); // Assuming data.courses is the string you want to log
+    } catch (error) {
+        console.error("Error fetching courses:", error);
+    }
+}
 
 export default function CourseDetails() {
     return (
-        <h1>course details bbg</h1>
+        <Button onClick={onClick}>pushme</Button>
     );
 }
