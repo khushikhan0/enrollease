ee2e6c60166c7c1415054ed836c1354b57c20823
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2024-11-03 03:05:06 -0500

    more chatbox styling

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "../App.css";
-import { Paper, TextField, Button, Chip, Stack, Card, CardContent, Typography, CardActions } from '@mui/material';
+import { TextField, Button, Card, CardContent, Box, CardActions } from '@mui/material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 export default function ChatBox() {
@@ -9,36 +9,45 @@ export default function ChatBox() {
 
     return (
         <div>
-            <p>{userInput}</p> 
-            <p>{aiInput}</p>
+            <Box display='flex' justifyContent='center' alignItems='center'>
+                <p>{userInput}</p> 
+                <p>{aiInput}</p>
 
-            <Card variant='outlined' sx={{ minWidth: '300px', display: 'flex', boxShadow: 'none', width: '90%', maxWidth: '600px', bottom: '5px' }}>
-                <CardContent sx={{ flexGrow: 1 }}>
-                        <TextField 
-                            label="ask advisor a question" 
-                            variant="outlined" 
-                            style={{
-                                margin: '6px',
-                                width: '100%',
-                            }}
-                            value={userInput}
-                            onChange={(e) => setUserInput(e.target.value)}
-                        />
-                </CardContent>
-                <CardActions sx={{ display: 'flex' }}>
-                    <Button size="small" sx={{ marginLeft: "-20px", display: 'flex' }}>
-                        <ArrowUpwardIcon 
-                            fontSize='large'
-                            sx={{
-                                backgroundColor: '#000000',  
-                                color: '#fff',             
-                                borderRadius: '20%',       
-                                padding: '4px'              
-                            }}
-                        />
-                    </Button>
-                </CardActions>
-            </Card>
+                <Card variant='contained' sx={{ boxShadow: 'none', display: 'flex', width: '90%', maxWidth: '1000px', borderRadius: '10px', backgroundColor: 'lightgray' }}>
+                    <Box display="flex" justifyContent="center" alignItems="center" height="100vh" width="100vw">
+                        <Card variant='outlined' sx={{ boxShadow: 'none', minWidth: '200px', display: 'flex', width: '80%', maxWidth: '1000px', marginBottom: '-80vh', borderRadius: '10px' }}>
+                            <CardContent sx={{ flexGrow: 1, borderRadius: '10px' }}>
+                                    <TextField 
+                                        label="ask advisor a question" 
+                                        variant="outlined" 
+                                        sx={{
+                                            margin: '6px',
+                                            width: '100%',
+                                            '& .MuiOutlinedInput-root': {
+                                                borderRadius: '10px'
+                                            }
+                                        }}
+                                        value={userInput}
+                                        onChange={(e) => setUserInput(e.target.value)}
+                                    />
+                            </CardContent>
+                                <Button size="small" sx={{ marginLeft: "-12px", display: 'flex', backgroundColor: "transparent", '&:focus': { outline: 'none' } }}>
+                                    <ArrowUpwardIcon 
+                                        fontSize='large'
+                                        sx={{
+                                            backgroundColor: 'lightgray',  
+                                            color: '#fff',             
+                                            borderRadius: '20%',       
+                                            padding: '4px',     
+                                            '&:hover': { backgroundColor: '#272727' },     
+                                        }}
+                                    />
+                                </Button>
+                        </Card>
+                    </Box>
+                </Card> 
+            </Box>
+           
         </div>
       );
 }
